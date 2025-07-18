e85ed771f3b0ced54fe2ade8cd4927d5e51f10c6
gajulavaishnavi-code <vyshnavi.gajula972@gmail.com>
2025-07-18 21:20:21 +0530

    the same change made

--- TheseIsDevWork/src/com/nt/main/TheDevCode.java
+++ TheseIsDevWork/src/com/nt/main/TheDevCode.java
@@ -5,6 +5,6 @@ public static void main(String[] args) {
 	System.out.println("Code developed by Develper");
 	System.out.println("The change made by the develoer");
   System.out.println("Hii these is TL" );
-  
+  System.out.println("Demo code");
 }
 }
