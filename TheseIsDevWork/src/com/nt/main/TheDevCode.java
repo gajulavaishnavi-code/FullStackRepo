3be5cde3e77dd589d2d9b49dcd56edfc928b5a6e
gajulavaishnavi-code <vyshnavi.gajula972@gmail.com>
2025-07-18 20:46:28 +0530

    developer changed

--- TheseIsDevWork/src/com/nt/main/TheDevCode.java
+++ TheseIsDevWork/src/com/nt/main/TheDevCode.java
@@ -3,5 +3,6 @@ package com.nt.main;
 public class TheDevCode {
 public static void main(String[] args) {
 	System.out.println("Code developed by Develper");
+	System.out.println("The change made ");
 }
 }
