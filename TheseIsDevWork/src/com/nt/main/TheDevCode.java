5a38194f15c4a69ea7751e5e9282a553a046c4f1
gajulavaishnavi-code <vyshnavi.gajula972@gmail.com>
2025-07-18 21:27:07 +0530

    branching demo

--- TheseIsDevWork/src/com/nt/main/TheDevCode.java
+++ TheseIsDevWork/src/com/nt/main/TheDevCode.java
@@ -5,6 +5,8 @@ public static void main(String[] args) {
 	System.out.println("Code developed by Develper");
 	System.out.println("The change made by the develoer");
   System.out.println("Hii these is TL" );
+  System.out.println("Demo code");
+  System.out.println("Branching");
   
 }
 }
