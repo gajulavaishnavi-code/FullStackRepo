c0247231c72dcfa029a364ef14b0cf4796ffe154
gajulavaishnavi-code <vyshnavi.gajula972@gmail.com>
2025-07-18 21:02:08 +0530

    added some more statement

--- TheseIsDevWork/src/com/nt/main/TheDevCode.java
+++ TheseIsDevWork/src/com/nt/main/TheDevCode.java
@@ -3,6 +3,7 @@ package com.nt.main;
 public class TheDevCode {
 public static void main(String[] args) {
 	System.out.println("Code developed by Develper");
-	System.out.println("The change made ");
+	System.out.println("The change made by the develoer");
+  System.out.println("Hii");
 }
 }
