2748bfcbdba9a27e11d2bf34f1bc5690e248b724
gajulavaishnavi-code <vyshnavi.gajula972@gmail.com>
2025-07-18 21:14:04 +0530

    made the statenent

--- TheseIsDevWork/src/com/nt/main/TheDevCode.java
+++ TheseIsDevWork/src/com/nt/main/TheDevCode.java
@@ -4,6 +4,7 @@ public class TheDevCode {
 public static void main(String[] args) {
 	System.out.println("Code developed by Develper");
 	System.out.println("The change made by the develoer");
-  System.out.println("Hii");
+  System.out.println("Hii these is TL" );
+  
 }
 }
